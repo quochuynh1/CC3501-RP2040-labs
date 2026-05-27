db1332740a27fa97f6082306c0771fbb1daf9caa
jd120219 <quoc.huynh1@my.jcu.edu.au>
2026-05-27 10:46:37 +1000

    Lab 2 - Experiment with the code to learn how to control multiple LEDs

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,7 @@
 #include "drivers/logging/logging.h"
 
 #define LED_PIN 14
+#define NUM_LEDS 12 
 
 int main()
 {
@@ -22,17 +23,34 @@ int main()
         log(LogLevel::INFORMATION, "Hello world");
 
         // Turn on the first LED to be a certain colour
-        uint8_t red = 0;
-        uint8_t green = 0;
-        uint8_t blue = 255;
-        led_data[0] = (red << 24) | (green << 16) | (blue << 8);
-        pio_sm_put_blocking(pio0, 0, led_data[0]);
+        // uint8_t red = 0;
+        // uint8_t green = 0;
+        // uint8_t blue = 255;
+        // led_data[0] = (green << 24) | (red << 16) | (blue << 8); // Update to align with WS2812 protocol from datasheet
+        // pio_sm_put_blocking(pio0, 0, led_data[0]);
+        // sleep_ms(500);
+
+        uint32_t led_data[NUM_LEDS]; // declare/initialise the led_data array
+
+        led_data[0] = (0 << 24) | (255 << 16) | (0 << 8); // red
+        led_data[1] = (255 << 24) | (0 << 16) | (0 << 8); // green
+        led_data[2] = (0 << 24) | (0 << 16) | (255 << 8); // blue
+        
+        for (int i=0; i < NUM_LEDS; i++) { // for each LED in the daisy-chain (0-11)
+            pio_sm_put_blocking(pio0, 0, led_data[i]); // send its 24-bit colour packet to the PIO
+        }
+        sleep_ms(500); 
+
+        for (int i=0; i < NUM_LEDS; i++) { // for each LED in the daisy-chain (0-11)
+            led_data[i] = 0; // set the led colour off
+            pio_sm_put_blocking(pio0, 0, led_data[i]); // send it to the PIO
+        }
         sleep_ms(500);
 
         // Set the first LED off 
-        led_data[0] = 0;
-        pio_sm_put_blocking(pio0, 0, led_data[0]);
-        sleep_ms(500);
+        // led_data[0] = 0;
+        // pio_sm_put_blocking(pio0, 0, led_data[0]);
+        // sleep_ms(500);
     }
 
     return 0;
